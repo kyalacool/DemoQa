13bb4b3f9a534aed0ff34c305496dfa655b6599d
Bence_Varga <bence.varga@nixs.com>
2025-10-30 11:55:47 +0100

    Refreshed BaseTest

--- src/test/java/com/automation/BaseTest.java
+++ src/test/java/com/automation/BaseTest.java
@@ -3,6 +3,7 @@ package com.automation;
 import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 
@@ -14,17 +15,13 @@ import static com.automation.utils.WebDriverManager.*;
 public class BaseTest {
     public WebDriver driver;
 
-    @BeforeClass
+    @BeforeMethod
     void setup() throws MalformedURLException {
         driver = setDriver();
-    }
-
-    @BeforeMethod
-    void startFromHomePage() {
         driver.get(getUrl());
     }
 
-    @AfterClass
+    @AfterMethod
     void tearDown() {
         driver.quit();
         removeThreadLocalDriver();
