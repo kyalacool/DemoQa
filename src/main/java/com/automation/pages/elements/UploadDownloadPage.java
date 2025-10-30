895494a0fca59a8d5b78654a5f971a37b1a1f853
Bence_Varga <bence.varga@nixs.com>
2025-10-30 10:07:24 +0100

    refreshed Jenkins

--- src/main/java/com/automation/pages/elements/UploadDownloadPage.java
+++ src/main/java/com/automation/pages/elements/UploadDownloadPage.java
@@ -15,7 +15,7 @@ import java.io.File;
 public class UploadDownloadPage extends BasePage {
 
     String fileName = "sampleFile.jpeg";
-    String downloadPath = "C:\\Users\\bence.varga\\Downloads\\" + fileName;
+    String downloadPath = "C:\\Users\\bence.varga\\IdeaProjects\\demoqa\\Downloads" + fileName;
 
     @Getter
     @FindBy(xpath = "//h1[@class='text-center' and contains(text(),'Upload and Download')]")
