13f8986409aaffc90aba282269a17cd3019aa825
Bence_Varga <bence.varga@nixs.com>
2025-10-31 17:17:40 +0100

    Allure

--- src/test/java/com/automation/ElementsPageTest.java
+++ src/test/java/com/automation/ElementsPageTest.java
@@ -1,10 +1,13 @@
 package com.automation;
 
+import io.qameta.allure.Step;
 import org.testng.annotations.Test;
 import com.automation.pages.HomePage;
 
+
 public class ElementsPageTest extends BaseTest {
 
+    @Step("Test case step")
     @Test
     void verifyTextBoxWithValidData() {
         HomePage homePage = new HomePage(driver);
